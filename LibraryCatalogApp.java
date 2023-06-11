ec52ad597d73dcda5c043a0cfd335d0cb09b158a
Surya Krishna <suryak614@gmail.com>
2023-06-12 00:10:13 +0530

    question1

--- LibraryCatalogApp.java
+++ LibraryCatalogApp.java
@@ -1,195 +1,192 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class LibraryCatalogApp {
-    private static final int MAX_BOOKS = 100;
+    private static final int MAX_BOOKS = 10; // Maximum number of books in the catalog
     private static final int BOOK_ID_INDEX = 0;
-    private static final int TITLE_INDEX = 1;
+    private static final int BOOK_TITLE_INDEX = 1;
     private static final int AUTHOR_INDEX = 2;
     private static final int AVAILABILITY_INDEX = 3;
     private static final int ISSUE_DATE_INDEX = 4;
+    private static final double CHARGE_PER_DAY = 10.0;
 
-    private String[][] catalog={ };
-    private int bookCount;
-    private Scanner scanner;
+    private static String[][] libraryCatalog = new String[MAX_BOOKS][5]; // Multi-dimensional array for books
 
-    public LibraryCatalogApp() {
-        catalog = new String[MAX_BOOKS][5];
-        bookCount = 0;
-        scanner = new Scanner(System.in);
-    }
-
-    public void run() {
+    public static void main(String[] args) {
+        initializeLibraryCatalog(); // Initialize the library catalog with sample data
         displayWelcomeMessage();
+
+        Scanner scanner = new Scanner(System.in);
         boolean exit = false;
         while (!exit) {
             displayMainMenu();
-            int option = getOption();
-            switch (option) {
+            System.out.print("Please choose an option from the above menu: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
                 case 1:
                     displayAllBooks();
                     break;
                 case 2:
-                    issueBook();
+                    issueBook(scanner);
                     break;
                 case 3:
-                    returnBook();
+                    returnBook(scanner);
                     break;
                 case 4:
                     exit = true;
                     break;
                 default:
                     System.out.println("Invalid option. Please try again.");
-                    break;
             }
+
+            System.out.println();
         }
-        scanner.close();
-        System.out.println("Thank you for using the Library Catalog App!");
+
+        System.out.println("Thank you for visiting the Library!");
     }
 
-    private void displayWelcomeMessage() {
+    private static void initializeLibraryCatalog() {
+        libraryCatalog[0] = new String[]{"101", "HTML and CSS", "Jon Duckett", "Available", "Null"};
+        libraryCatalog[1] = new String[]{"102", "JavaScript: The Good Parts", "Douglas C", "Available", "Null"};
+        libraryCatalog[2] = new String[]{"103", "Learning Web Design", "Jennifer N", "CP2014", "23-May-2023"};
+        libraryCatalog[3] = new String[]{"104", "Responsive Web Design", "Ben Frain", "EC3142", "17-May-2023"};
+    }
+
+    private static void displayWelcomeMessage() {
         System.out.println("--------------------------------------------------");
         System.out.println("Welcome to the Unique Library");
         System.out.println("--------------------------------------------------");
     }
 
-    private void displayMainMenu() {
+    private static void displayMainMenu() {
         System.out.println("View the complete list of Books");
         System.out.println("Issue a Book");
         System.out.println("Return a Book");
         System.out.println("Exit");
-        System.out.print("Please choose an option from the above menu: ");
     }
 
-    private int getOption() {
-        try {
-            return Integer.parseInt(scanner.nextLine().trim());
-        } catch (NumberFormatException e) {
-            return -1;
-        }
-    }
-
-    private void displayAllBooks() {
-        System.out.println("\n--------------------------------------------------");
+    private static void displayAllBooks() {
+        System.out.println("--------------------------------------------------");
         System.out.println("List of all Books");
         System.out.println("--------------------------------------------------");
-        System.out.println("Book ID\t\tBook Title\t\tAuthor\t\tAvailability\tIssue Date");
-        for (int i = 0; i < bookCount; i++) {
-            System.out.printf("%s\t\t%s\t\t%s\t\t%s\t\t%s%n",
-                    catalog[i][BOOK_ID_INDEX],
-                    catalog[i][TITLE_INDEX],
-                    catalog[i][AUTHOR_INDEX],
-                    catalog[i][AVAILABILITY_INDEX],
-                    catalog[i][ISSUE_DATE_INDEX]);
+        System.out.printf("%-8s%-20s%-15s%-15s%-15s%n",
+                "Book ID", "Book Title", "Author", "Availability", "Issue Date");
+        for (String[] book : libraryCatalog) {
+            System.out.printf("%-8s%-20s%-15s%-15s%-15s%n",
+                    book[BOOK_ID_INDEX], book[BOOK_TITLE_INDEX], book[AUTHOR_INDEX],
+                    book[AVAILABILITY_INDEX], book[ISSUE_DATE_INDEX]);
         }
         System.out.println();
-        System.out.print("Enter 'Y' to return to the main menu or 'N' to Exit: ");
-        String choice = scanner.nextLine().trim();
-        if (!choice.equalsIgnoreCase("Y")) {
-            System.exit(0);
-        }
     }
 
-    private void issueBook() {
-        System.out.println("\n--------------------------------------------------");
+    private static void issueBook(Scanner scanner) {
+        System.out.println("--------------------------------------------------");
         System.out.println("Issue the Book");
         System.out.println("--------------------------------------------------");
         System.out.print("Enter the Book ID: ");
-        String bookId = scanner.nextLine().trim();
-        int bookIndex = findBookIndex(bookId);
-        if (bookIndex == -1) {
-            System.out.println("Book not found.");
-            return;
-        }
-        String availability = catalog[bookIndex][AVAILABILITY_INDEX];
-        if (!availability.equalsIgnoreCase("available")) {
-            System.out.println("Book is already issued to a student.");
-            return;
-        }
-        System.out.println(bookId + " - " + catalog[bookIndex][TITLE_INDEX] + " by " + catalog[bookIndex][AUTHOR_INDEX] + ": Available");
-        System.out.print("Enter Student ID: ");
-        String studentId = scanner.nextLine().trim();
-        System.out.print("Enter 'C' to confirm: ");
-        String confirmation = scanner.nextLine().trim();
-        if (confirmation.equalsIgnoreCase("C")) {
-            catalog[bookIndex][AVAILABILITY_INDEX] = studentId;
-            catalog[bookIndex][ISSUE_DATE_INDEX] = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
-            System.out.println("BookID: " + bookId + " is issued to " + studentId);
+        String bookId = scanner.nextLine();
+
+        int bookIndex = findBookIndexById(bookId);
+        if (bookIndex != -1) {
+            String[] book = libraryCatalog[bookIndex];
+            String bookTitle = book[BOOK_TITLE_INDEX];
+            String availabilityStatus = book[AVAILABILITY_INDEX];
+            String issueDate = book[ISSUE_DATE_INDEX];
+
+            if (availabilityStatus.equals("Available")) {
+                System.out.println(bookId + " " + bookTitle + " - " + book[AUTHOR_INDEX] + ": " + availabilityStatus);
+                System.out.print("Enter StudentID: ");
+                String studentId = scanner.nextLine();
+                System.out.print("Enter 'C' to confirm: ");
+                String confirmation = scanner.nextLine();
+
+                if (confirmation.equalsIgnoreCase("C")) {
+                    book[AVAILABILITY_INDEX] = studentId;
+                    book[ISSUE_DATE_INDEX] = getCurrentDate();
+                    System.out.println("BookID: " + bookId + " is Issued to " + studentId);
+                } else {
+                    System.out.println("Issue process canceled.");
+                }
+            } else {
+                System.out.println("The book is not available for issuing.");
+            }
         } else {
-            System.out.println("Issue canceled.");
-        }
-        System.out.print("Enter 'Y' to return to the main menu or 'N' to Exit: ");
-        String choice = scanner.nextLine().trim();
-        if (!choice.equalsIgnoreCase("Y")) {
-            System.exit(0);
+            System.out.println("Book with ID " + bookId + " not found.");
         }
     }
 
-    private void returnBook() {
-        System.out.println("\n--------------------------------------------------");
+    private static void returnBook(Scanner scanner) {
+        System.out.println("--------------------------------------------------");
         System.out.println("Return the Book");
         System.out.println("--------------------------------------------------");
         System.out.print("Enter the Book ID: ");
-        String bookId = scanner.nextLine().trim();
-        int bookIndex = findBookIndex(bookId);
-        if (bookIndex == -1) {
-            System.out.println("Book not found.");
-            return;
-        }
-        String availability = catalog[bookIndex][AVAILABILITY_INDEX];
-        if (availability.equalsIgnoreCase("available")) {
-            System.out.println("Book is already available in the library.");
-            return;
-        }
-        System.out.println(bookId + " - " + catalog[bookIndex][TITLE_INDEX] + " by " + catalog[bookIndex][AUTHOR_INDEX]);
-        System.out.println("StudentID - " + availability);
-        System.out.println("Issue Date - " + catalog[bookIndex][ISSUE_DATE_INDEX]);
-        long daysDelayed = calculateDaysDelayed(catalog[bookIndex][ISSUE_DATE_INDEX]);
-        if (daysDelayed <= 0) {
-            System.out.println("No delayed charges.");
+        String bookId = scanner.nextLine();
+
+        int bookIndex = findBookIndexById(bookId);
+        if (bookIndex != -1) {
+            String[] book = libraryCatalog[bookIndex];
+            String availabilityStatus = book[AVAILABILITY_INDEX];
+            String issueDate = book[ISSUE_DATE_INDEX];
+
+            if (!availabilityStatus.equals("Available")) {
+                System.out.println(bookId + " - " + book[BOOK_TITLE_INDEX]);
+                System.out.println("StudentID - " + availabilityStatus);
+                System.out.println("Issue Date - " + issueDate);
+
+                int delayedDays = calculateDelayedDays(issueDate);
+                if (delayedDays > 0) {
+                    double delayedCharges = delayedDays * CHARGE_PER_DAY;
+                    System.out.println("Delayed by - " + delayedDays + " days");
+                    System.out.println("Delayed Charges - Rs. " + delayedCharges);
+                }
+
+                System.out.print("Enter 'R' to confirm the return: ");
+                String confirmation = scanner.nextLine();
+
+                if (confirmation.equalsIgnoreCase("R")) {
+                    book[AVAILABILITY_INDEX] = "Available";
+                    book[ISSUE_DATE_INDEX] = "Null";
+                    System.out.println("BookID: " + bookId + " is returned back");
+                } else {
+                    System.out.println("Return process canceled.");
+                }
+            } else {
+                System.out.println("The book is not issued to anyone.");
+            }
         } else {
-            double charges = calculateCharges(daysDelayed);
-            System.out.printf("Delayed by - %d days%n", daysDelayed);
-            System.out.printf("Delayed Charges - Rs. %.2f%n", charges);
-        }
-        System.out.print("Enter 'R' to confirm the return: ");
-        String confirmation = scanner.nextLine().trim();
-        if (confirmation.equalsIgnoreCase("R")) {
-            catalog[bookIndex][AVAILABILITY_INDEX] = "available";
-            System.out.println("BookID: " + bookId + " is returned back");
-        } else {
-            System.out.println("Return canceled.");
-        }
-        System.out.print("Enter 'Y' to return to the main menu or 'N' to Exit: ");
-        String choice = scanner.nextLine().trim();
-        if (!choice.equalsIgnoreCase("Y")) {
-            System.exit(0);
+            System.out.println("Book with ID " + bookId + " not found.");
         }
     }
 
-    private int findBookIndex(String bookId) {
-        for (int i = 0; i < bookCount; i++) {
-            if (catalog[i][BOOK_ID_INDEX].equalsIgnoreCase(bookId)) {
+    private static int findBookIndexById(String bookId) {
+        for (int i = 0; i < libraryCatalog.length; i++) {
+            if (libraryCatalog[i][BOOK_ID_INDEX].equals(bookId)) {
                 return i;
             }
         }
         return -1;
     }
 
-    private long calculateDaysDelayed(String issueDateStr) {
-        LocalDate issueDate = LocalDate.parse(issueDateStr, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
-        LocalDate currentDate = LocalDate.now();
-        return ChronoUnit.DAYS.between(issueDate, currentDate) - 7;
+    private static String getCurrentDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+        return sdf.format(new Date());
     }
 
-    private double calculateCharges(long daysDelayed) {
-        return daysDelayed * 10.0;
-    }
+    private static int calculateDelayedDays(String issueDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+        try {
+            Date currentDate = sdf.parse(getCurrentDate());
+            Date dateIssued = sdf.parse(issueDate);
 
-    public static void main(String[] args) {
-        LibraryCatalogApp app = new LibraryCatalogApp();
-        app.run();
+            long difference = currentDate.getTime() - dateIssued.getTime();
+            long daysDifference = difference / (1000 * 60 * 60 * 24);
+            return (int) daysDifference - 7;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
